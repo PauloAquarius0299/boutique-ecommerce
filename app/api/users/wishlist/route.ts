d2e8e1549b620a8c04d23687e7188ac259581ff4
Paulo Cesar <paulocgsantos99@gmail.com>
2024-06-21 10:40:56 -0300

    fetch add producinfo

--- app/api/users/wishlist/route.ts
+++ app/api/users/wishlist/route.ts
@@ -1,41 +1,47 @@
-import {NextRequest, NextResponse} from 'next/server';
-import {auth} from '@clerk/nextjs/server'
-import {connectToDB} from '@/lib/mongoDB';
-import User from '@/lib/models/User'
+import User from "@/lib/models/User";
+import { connectToDB } from "@/lib/mongoDB";
+
+import { auth } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
+import { NextRequest, NextResponse } from "next/server";
 
 export const POST = async (req: NextRequest) => {
-    try{
-        const {userId} = auth()
+  try {
+    const { userId } = auth()
 
-        if(!userId){
-            return new NextResponse('Unauthorized', {status:401})
-        }
-
-        await connectToDB()
-
-        const user = await User.findOne({clerkId: userId})
-
-        if (!user){
-            return new NextResponse('Usuario não encontrado', {status: 404})
-        }
-
-        const {productId} = await req.json()
-
-        if(!productId){
-            return new NextResponse('Product id required', {status:400})
-        }
-
-        const isLiked = user.wishlist.includes(productId)
-
-        if(isLiked){
-            //dislike
-            user.wishlist= user.wishlist.filter((id: string) => id !== productId)
-        } else{
-            //like
-            user.wishlist.push(productId)
-        }
-    } catch(err){
-        console.log("[wishilist_POST]", err);
-        return new NextResponse('Internal Server Error', {status: 500})
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 })
     }
+
+    await connectToDB()
+
+    const user = await User.findOne({ clerkId: userId })
+
+    if (!user) {
+      return new NextResponse("User not found", { status: 404 })
+    }
+
+    const { productId } = await req.json()
+
+    if (!productId) {
+      return new NextResponse("Product Id required", { status: 400 })
+    }
+
+    const isLiked = user.wishlist.includes(productId)
+
+    if (isLiked) {
+      // Dislike
+      user.wishlist = user.wishlist.filter((id: string) => id !== productId)
+    } else {
+      // Like
+      user.wishlist.push(productId)
+    }
+
+    await user.save()
+    
+    return NextResponse.json(user, { status: 200 })
+  } catch (err) {
+    console.log("[wishlist_POST]", err);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
 }
--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,59 +1,10 @@
 "use client"
 
-import { Heart } from 'lucide-react';
+import HeartFavorite from './HeartFavorite'
 import Image from "next/image";
 import Link from "next/link";
-import {useUser} from '@clerk/nextjs'
-import {useState, useEffect} from 'react'
-import { useRouter } from 'next/navigation';
 
 const ProductCard = ({ product}: {product: ProductType} ) => {
-  const router = useRouter();
-  const {user} = useUser();
-
-  const [loading, setLoading] = useState(false)
-  const [signedInUser, setSignedInUser] = useState<UserType | null>(null);
-  const [isLiked, setIsLiked] = useState(false)
-
-  const getUser = async ()=> {
-    try{
-      setLoading(true)
-      const res = await fetch("/api/users")
-      const data = await res.json()
-      setSignedInUser(data)
-      setIsLiked(data.wishlist.includes(product._id))
-      setLoading(false)
-    } catch(err){
-      console.log("[users_GET]", err);
-    }
-  }
-
-  useEffect(()=> {
-    if(user){
-      getUser()
-    }
-  }, [user])
-
-  const handleLike = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    e.preventDefault()
-
-    try{
-      if(!user){
-        router.push("/sign-in")
-      } else {
-      setLoading(true)
-      const res = await fetch("/api/users/wishlist", {
-        method: 'POST',
-        body: JSON.stringify({productId: product._id}),
-      })
-      const updatedUser = await res.json()
-      setSignedInUser(updatedUser)
-      setIsLiked(updatedUser.wishlist.includes(product._id))
-      }
-    } catch (err){
-      console.log("[wishlist_POST]", err);
-    }
-  }
 
   return (
     <Link
@@ -73,9 +24,7 @@ const ProductCard = ({ product}: {product: ProductType} ) => {
       </div>
       <div className="flex justify-between items-center">
         <p className="text-body-bold">${product.price}</p>
-       <button onClick={handleLike}>
-        <Heart fill={`${isLiked ? "red" : "white" }`} />
-       </button>
+        <HeartFavorite product={product}  />
       </div>
     </Link>
   );
